e8846497715dcd89ca8f0dff162600b47e4aa88d
Torrie Fischer <torrie@ripple.com>
2014-10-03 13:57:32 -0700

    Support bigger files in the cpp output

--- compile-embedded-resources.py
+++ compile-embedded-resources.py
@@ -5,16 +5,30 @@ import os
 root = sys.argv[1]
 
 out = open(sys.argv[2], 'w')
-out.write("static const std::map<std::string, std::pair<std::vector<unsigned char>, size_t> > contents = {\n")
 
+fileMap = {}
+fileIdx = 0
+
+out.write("static const char* file_contents[] = {\n")
 for path, dirs, files in os.walk(sys.argv[1]):
   for f in files:
     relPath = unicode('/'.join((path.replace(root, '', 1), f)))
     if relPath.startswith('//'):
       relPath = relPath[1:]
     contents = open('/'.join((path, f)), 'r').read()
-    out.write('{"%s", {{\n'%(relPath))
-    out.write(','.join(map(lambda x:str(ord(x)), contents)))
-    out.write('}, %d}},\n' % (len(contents)))
+    fileMap[fileIdx] = {'path': relPath, 'size': len(contents)}
+    fileIdx += 1
+    out.write('"'+''.join(map(lambda x:'\\'+hex(ord(x))[1:], contents))+'",\n')
 
-out.write("};\n ")
+out.write("0};\n\n")
+
+out.write("\n".join((
+           "typedef struct {",
+           "    std::uint64_t size;",
+           "    const char* contents;",
+           "} file_entry;",
+           "static const std::map<std::string, file_entry> contents = {","")))
+for idx,contents in fileMap.iteritems():
+  out.write('    { "%s", {%s, file_contents[%d]}},\n'%(contents['path'],
+  contents['size'], idx))
+out.write("};\n")
